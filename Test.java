9ebbeac2975156a57f8910fa00a44e4f6c865568
rishabh-20 <narayanrishabh2011@gmail.com>
2024-05-17 14:43:24 +0530

    Added Test.java

--- Test.java
+++ Test.java
@@ -3,3 +3,11 @@ public class Test{
       System.out.println("Hello World"):
   }
 }
+
+public class Demo{
+   Public Static void main(String[] args){
+      System.out.println("Welcome To Java");
+   }
+   
+
+}
